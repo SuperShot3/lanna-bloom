af7566757cb21f2cbdeef3d7f9a78ca3e1ef3335
bi <bi@bis-MacBook-Pro.local>
2026-07-18 20:42:07 +0500

    google_accouunt

--- lib/reviewsConfig.ts
+++ lib/reviewsConfig.ts
@@ -10,6 +10,6 @@ export const GOOGLE_BUSINESS_RATING = Number(
 );
 
 export const GOOGLE_BUSINESS_REVIEW_COUNT = Number(
-  process.env.NEXT_PUBLIC_GOOGLE_BUSINESS_REVIEW_COUNT ?? '17'
+  process.env.NEXT_PUBLIC_GOOGLE_BUSINESS_REVIEW_COUNT ?? '20'
 );
   
